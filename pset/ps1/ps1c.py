6a27c6f98662eb386a02441d19c30ba10b29815c
katnip <kanijupiter@gmail.com>
2023-04-04 18:00:11 +0300

    sync

--- pset/ps1/ps1c.py
+++ pset/ps1/ps1c.py
@@ -1,15 +1,17 @@
-houseCost = 1_000_000
-currentSavings = 0
-monthsLimit = 36
+def main():
+    houseCost = 1_000_000
+    downPaymentRate = 0.25
+    totalCost = houseCost * downPaymentRate
 
-semiAnnualRaise = 0.07
-annualReturn = 0.04
+    semiAnnualRaise = 0.07
+    annualReturn = 0.04
 
-downPaymentRate = 0.25
-totalCost = houseCost * downPaymentRate
+    annualSalary = float(input("Enter the starting salary: "))
+    monthlySalary = annualSalary / 12
 
-annualSalary = float(input("Enter the starting salary: "))
-monthlySalary = annualSalary / 12
+    savingsRate, steps = getOptimalSavingsRate(monthlySalary, annualReturn, semiAnnualRaise, totalCost)
+    print(f"Best savings rate: {savingsRate}")
+    print(f"Steps in bisection search: {steps}")
 
 
 def calcSavingsAfter36Months(monthlySalary, savingsRate, annualReturn, semiAnnualRaise):
@@ -30,6 +32,7 @@ def calcSavingsAfter36Months(monthlySalary, savingsRate, annualReturn, semiAnnua
 
     return currentSavings
 
+
 def getOptimalSavingsRate(monthlySalary, annualReturn, semiAnnualRaise, totalCost):
     savingsRate = 0.00
     currentSavings = 0
@@ -37,8 +40,10 @@ def getOptimalSavingsRate(monthlySalary, annualReturn, semiAnnualRaise, totalCos
     low = 0
     high = 10000
     guess = (low + high) / 2
+    steps = 0
 
     while abs(currentSavings - totalCost) >= epsilon:
+        steps += 1
         savingsRate = guess / 100.0
         currentSavings = calcSavingsAfter36Months(monthlySalary, savingsRate, annualReturn, semiAnnualRaise)
 
@@ -50,4 +55,8 @@ def getOptimalSavingsRate(monthlySalary, annualReturn, semiAnnualRaise, totalCos
             break
         guess = (low + high) / 2
 
-    return savingsRate
+    return savingsRate, steps
+
+
+if __name__ == '__main__':
+    main()
